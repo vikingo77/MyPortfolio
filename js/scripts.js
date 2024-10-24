e1bbe7459f54c9487395b467842f67e3b3432724
Victor MFB <victormanuelfbi11@gmail.com>
2024-10-24 08:23:02 -0400

    Terminado y listo para desplegar en github

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,33 @@ const toggleColors = document.getElementById('toggle-colors');
 /*Vamos a trakear a nuestras variables del css*/
 const rootStyles = document.documentElement.style; // aqui estamos accediendo a todas las variables css
 
+/*Agregaremos el json para el cambio de idioma*/
+const flagsElement = document.getElementById('flags');
+
+const textsToChange = document.querySelectorAll('[data-section]');
+
+const changeLanguage = async (language) => {
+    try {
+        const requestJson = await fetch(`./languages/${language}.json`);
+        const text = await requestJson.json();
+
+        // Itera sobre textsToChange correctamente
+        for (const element of textsToChange) {
+            const section = element.dataset.section;
+            const value = element.dataset.value;
+
+            element.innerHTML = text[section][value];
+        }
+    } catch (error) {
+        console.error('Error al cambiar el idioma:', error);
+    }
+};
+
+
+
+flagsElement.addEventListener('click', (e) => {
+    changeLanguage(e.target.parentElement.dataset.language);
+});
 
 toggleTheme.addEventListener('click', () => {
     document.body.classList.toggle('dark');
